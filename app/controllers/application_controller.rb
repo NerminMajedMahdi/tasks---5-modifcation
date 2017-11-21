3fe2eef3e9c3bd3c6e89482259b8ea672478d06b
NerminMajedMahdi <mahdi.nirmeen@gmail.com>
2017-11-21 17:08:04 +0200

    5.5 revesion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+   rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, alert: exception.message
+  end
+
+end
+
+
 
 
   # adding field first_name  last_name to devise
@@ -11,20 +18,7 @@ class ApplicationController < ActionController::Base
      # devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
    # end
 
- module API
-  extend ActiveSupport::Concern
 
-  included do
-    rescue_from ::CanCan::AccessDenied do
-      error!('403 Forbidden', 403)
-    end
-  end
-  # Convienience method to authorize every route
-  def authorize_routes!
-    before { authorize_route! }
-  end
-end
-  end
 
 
 
