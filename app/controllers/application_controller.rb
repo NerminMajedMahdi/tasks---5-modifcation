d8fbc510f98d63b85ff1b9eccc2dddfb0839302d
NerminMajedMahdi <mahdi.nirmeen@gmail.com>
2017-11-22 05:31:40 +0200

    5.7 comment task

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to main_app.root_url, alert: exception.message
-  end
 
 end
 
@@ -22,15 +19,6 @@ end
 
 
 
-
-def index
-  if params[:q]
-    search_term = params[:q]
-    @products = Product.search(search_term)
-  else
-    @products = Product.all
-  end
-end
 
 
 
